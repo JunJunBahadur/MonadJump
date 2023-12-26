373dc7eaf7cbe894fc715623b5faab098c274196
Arjun Senthil <arjunsenthil.dev@gmail.com>
2023-12-26 14:25:33 +0530

    added score and speed mech

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 const cursor = document.querySelector('.cursor');
 const holes = [...document.querySelectorAll('.hole')];
 const scoreEl = document.querySelector('.score span');
+const speedMeter = document.querySelector('.score p');
+const HighScoreEl = document.querySelector('.highscore span');
+const playButton = document.querySelector('.play');
+
+const board = document.querySelector('.board');
+
+playButton.style.visibility = 'visible';
+board.style.visibility = 'hidden';
 let score = 0;
+let speed = 1500;
+speedMeter.textContent = speed;
 
 const sound = new Audio("sprites/smash.mp3");
 
+function start(){
+    playButton.style.visibility = 'hidden';
+    board.style.visibility = 'visible';
+    scoreEl.textContent = score;
+    run();
+}
+
 function run(){
+
+    let over = true;
+
     const i = Math.floor(Math.random()*holes.length);
     const hole = holes[i];
     let timer = null;
@@ -13,6 +33,19 @@ function run(){
     const img = document.createElement('img');
     img.classList.add('mole');
     img.src = 'sprites/mole.png';
+
+    if(score > 70){
+        speed = 500;
+    } else if(score > 50){
+        speed = 700;
+    } else if(score > 30){
+        speed = 900;
+    }else if(score > 15){
+        speed = 1100;
+    }else if(score> 5){
+        speed = 1300;
+    }
+    speedMeter.textContent = speed;
 
     img.addEventListener('click', ()=> {
         score++;
@@ -27,12 +60,29 @@ function run(){
     })
 
     hole.appendChild(img);
+    
+    console.log(speed);
+    
+    
     timer = setTimeout(() => {
         hole.removeChild(img);
-        run();
-    },1500);
+        console.log(over);
+        gameover();
+    },speed);
+    
 }
-run()
+//run()
+
+function gameover(){
+    playButton.style.visibility = 'visible';
+    board.style.visibility = 'hidden';
+    curHS = HighScoreEl.textContent;
+    if(curHS<score){
+        HighScoreEl.textContent = score;
+    }
+    score = 0;
+    speed = 1500;
+}
 
 window.addEventListener('mousemove', e=> {
     cursor.style.top = e.pageY + 'px';
