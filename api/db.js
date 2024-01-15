c993417ba069eb8a6b9ff874fbbc8233f427d53f
Arjun Senthil <arjunsenthil.dev@gmail.com>
2024-01-15 15:57:09 +0530

    server done

--- api/db.js
+++ api/db.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose')
+const express = require('express');
+const Router = require('./routes');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
 
 const url = `mongodb+srv://arjunsenthildev:WYGUn1RYCc1i7kHL@bonknads.ctsh71u.mongodb.net/?retryWrites=true&w=majority`;
 
-const connectionParams={
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true 
-}
-mongoose.connect(url,connectionParams)
+mongoose.connect(url)
     .then( () => {
         console.log('Connected to the database ')
     })
     .catch( (err) => {
         console.error(`Error connecting to the database. n${err}`);
-    })
+    });
+
+app.use(Router);
+app.listen(3000, () => {
+    console.log('Server at port 3000');
+});
