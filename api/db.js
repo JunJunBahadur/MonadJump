e87ac698068932f143f043a2f0d672be64522ef2
Arjun Senthil <arjunsenthil.dev@gmail.com>
2024-01-19 12:13:16 +0530

    added discord auth

--- api/db.js
+++ api/db.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const Router = require('./routes');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+let PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
 
 const url = `mongodb+srv://arjunsenthildev:WYGUn1RYCc1i7kHL@bonknads.ctsh71u.mongodb.net/?retryWrites=true&w=majority`;
 
+/*
 mongoose.connect(url)
     .then( () => {
         console.log('Connected to the database ')
@@ -14,8 +18,8 @@ mongoose.connect(url)
     .catch( (err) => {
         console.error(`Error connecting to the database. n${err}`);
     });
-
+*/
 app.use(Router);
-app.listen(3000, () => {
-    console.log('Server at port 3000');
+app.listen(PORT, () => {
+    console.log('Server at port ',PORT);
 });
--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,62 @@
 const express = require('express');
 const userModel = require('./model');
 const app = express();
+const url = require('url');
+const axios = require('axios');
+require('dotenv').config();
+
+
+app.get('/auth/discord/redirect', async (req, res) => {
+    const { code } = req.query;
+
+    if (code) {
+        const formData = new url.URLSearchParams({
+            client_id: process.env.ClientID,
+            client_secret: process.env.ClientSecret,
+            grant_type: 'authorization_code',
+            code: code.toString(),
+            redirect_uri: 'http://localhost:3000/auth/discord/redirect',
+        });
+
+        const output = await axios.post('https://discord.com/api/v10/oauth2/token', 
+            formData, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }
+        );
+
+        if(output.data) {
+            const access = output.data.access_token;
+
+            const userInfo = await axios.get('https://discord.com/api/v10/users/@me', {
+                headers: {
+                    'Authorization': `Bearer ${access}`,
+                },
+            });
+
+            //refresh
+
+            const formDataRe = new url.URLSearchParams({
+                client_id: process.env.ClientID,
+                client_secret: process.env.ClientSecret,
+                grant_type: 'refresh_token',
+                refresh_token: output.data.refresh_token,
+            });
+    
+            const refresh = await axios.post('https://discord.com/api/v10/oauth2/token', 
+                formDataRe, {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                }
+            );
+
+            console.log(output.data, userInfo.data, refresh.data);
+
+        }
+    }
+});
 
 app.post('/add', async (request, response) => {
     const user = new userModel(request.body);
